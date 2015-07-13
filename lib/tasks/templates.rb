648d68bc7cc0dfcde8028b1f85e0fa18bec35f7d
David Havard <david.havard@mrzen.com>
2015-07-13 12:07:57 +0100

    Updated template helper to build all less templates

--- lib/tasks/templates.rb
+++ lib/tasks/templates.rb
@@ -43,8 +43,8 @@ def build_template(template_name)
   end
 
   # Process any LESS files
-  if $package['less'].keys.include? template_name
-      compile_less_styles(template_build_area, $package['less'][template_name])
+  $package['less'].keys.each do |file|
+    compile_less_styles(template_build_area, $package['less'][file])
   end
 
   chdir(template_build_area) do

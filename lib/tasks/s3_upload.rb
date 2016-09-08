7f3464ad1e6278dd7620adca94866ad2166c8a01
Leo Adamek <leo.adamek@mrzen.com>
2016-09-08 15:39:17 +0100

    v2.0 -- Upgraded to aws-sdk 2.0

--- lib/tasks/s3_upload.rb
+++ lib/tasks/s3_upload.rb
@@ -18,9 +18,8 @@ desc 'Upload the package to S3'
 task :upload => [:package] do
 
   require 'aws-sdk'
-
-  s3 = Aws::S3.new(s3_credentials)
-  bucket = s3.buckets[s3_bucket]
+  s3 = Aws::S3::Client.new(s3_credentials)
+  #bucket = s3.bucket(s3_bucket)
 
   package_files = [
            File.join( $package['s3']['path'] , package_name + '.zip'),
@@ -28,14 +27,22 @@ task :upload => [:package] do
           ]
 
   package_files.each do |file|
-    o = bucket.objects[file]
-    o.write(Pathname.new( File.join('.' , 'packages', package_name + '.zip' )))
-    o.acl = :public_read
+    File.open(Pathname.new( File.join('.' , 'packages', package_name + '.zip' )), 'rb') do |f|
+      s3.put_object(
+        acl: 'public-read',
+        body: f,
+        bucket: s3_bucket,
+        key: file,
+      )
+    end
   end
 
-  o = bucket.objects[ File.join($package['s3']['path'] , 'updates.xml') ]
-  o.write(update_manifest)
-  o.acl = :public_read
+  s3.put_object(
+    bucket: s3_bucket,
+    key: File.join($package['s3']['path'] , 'updates.xml'),
+    acl: 'public-read',
+    body: update_manifest
+    )
 
   p "Uploaded package #{package_name}.zip and update manifest to S3"
 
